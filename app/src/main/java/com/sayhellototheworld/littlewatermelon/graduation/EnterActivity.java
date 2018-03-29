dd02cdbb0b97ea58e921c4680f3bcde3e4fe6d86
Little WatermelonSeed <15528152439@163.com>
2018-03-29 23:32:14 +0800

    Initial commit

---
+++ app/src/main/java/com/sayhellototheworld/littlewatermelon/graduation/EnterActivity.java
@@ -0,0 +1,14 @@
+package com.sayhellototheworld.littlewatermelon.graduation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+    }
+
+}
